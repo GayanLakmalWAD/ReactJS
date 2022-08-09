fb7688986f69bcf7affc11cfd0b2b44c827799e9
Gayan Lakmal <gayanl@vega.lk>
2022-08-09 18:00:03 +0530

    Added my project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Main from '../src/components/Main'
+import NavBar from '../src/components/NavBar'
+
+export default function App() {
+    return (
+        <div className='container'>
+            <NavBar />
+            <Main />
+        </div>
+    )
+}
